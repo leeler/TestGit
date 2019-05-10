dea68694d26f9a243b4f89bef11facf0116783ef
leeler <leelert@163.com>
2019-05-10 16:44:44 +0800

    firt commit

---
+++ src/main/java/com/leeler/TestGit.java
@@ -0,0 +1,18 @@
+ /*
+  *Copyright (c) 2003, 2018, UMF and/or its affiliates. All rights reserved.
+  *
+  */
+ package com.leeler;
+
+ /**
+  * @program: TestGit
+  * @description: TODO
+  * @author: lile
+  * @create: 2019-05-10 16:38
+  **/
+ public class TestGit {
+
+     public static void main(String[] args) {
+         System.out.println("Hello Git");
+     }
+ }
